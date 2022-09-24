b2b40d87fb5ba06bcbbc2d5d92f39a2243d0cac0
olenka10122 <olenka10122@gmail.com>
2022-09-24 12:46:24 +0300

    Добавила изменения вторго занятия

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-console.log('Hello world!');
+console.log("Hello world!");
 
-const ourPlanetName = 'Земля';
+const ourPlanetName = "Земля";
 console.log(ourPlanetName);
 
-let MyTime = '19:57';
+let MyTime = "19:57";
 console.log(MyTime);
 
 let NumberOfArticles = 5;
 console.log(NumberOfArticles);
 
-let CashlessPayment = 'Наличные';
-let Cash = 'Безналичные';
+let CashlessPayment = "Наличные";
+let Cash = "Безналичные";
 console.log(Cash);
 
-let Surname = 'Зотова', Name = 'Ольга', Patronymic = 'Викторовна';
+const Surname = "Зотова",
+  Name = "Ольга",
+  Patronymic = "Викторовна";
 console.log(Surname, Name, Patronymic);
+
+const age = 46;
+const name = 'Иван';
+const userData = `Имя пользователя: ${name}, возраст ${age}`;
+const userDataNew = 'Имя пользователя: ${name}, возраст ${age}';
+console.log(userData);
+
+const isUser = false;
+const isVisible = true;
+
+let townName;
+console.log(townName);
+
+townName = 'Moscow';
+console.log(townName);
+
+
+const userDate = {
+  Name: 'Ivan',
+  age: 34,
+};
+console.log(userDate.Name);
+
+console.log(typeof userDate);
+console.log(typeof userDate.Name);
+console.log(typeof userDate.age);
+
