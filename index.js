bfff0b4b75a6c5595564f4d3dc3d877cb0789a41
olenka10122 <olenka10122@gmail.com>
2022-09-24 10:38:52 +0300

    Добавила файлы первого занятия

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('Hello world!');
+
+const ourPlanetName = 'Земля';
+console.log(ourPlanetName);
+
+let MyTime = '19:57';
+console.log(MyTime);
+
+let NumberOfArticles = 5;
+console.log(NumberOfArticles);
+
+let CashlessPayment = 'Наличные';
+let Cash = 'Безналичные';
+console.log(Cash);
+
+let Surname = 'Зотова', Name = 'Ольга', Patronymic = 'Викторовна';
+console.log(Surname, Name, Patronymic);
