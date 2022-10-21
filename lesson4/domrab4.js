01011e2a9c713d4f8b35849c41048486f28f565c
olenka10122 <olenka10122@gmail.com>
2022-10-21 21:13:20 +0300

    домашняя работа 4

---
+++ lesson4/domrab4.js
@@ -0,0 +1,45 @@
+/* function quadro (a, b){
+    return a ** b;
+}
+console.log(quadro(2, 2));
+
+
+summ(3, 7);
+function summ(a, b) {
+    console.log(a + b);
+} */
+
+
+/* function uravn(a, b, c) {
+    return (a - b) / c;
+}
+console.log(uravn(10, 2, 2)) */
+
+
+/* function week(a) {
+    return a = 1;
+    }
+const result = 'Понедельник';
+
+    console.log(result); */
+
+    
+
+/*     function ravenstvo(a, b){
+        return a === b; {
+            return true;
+        }
+    }
+    console.log(ravenstvo(2, 2));
+    console.log(ravenstvo(2, 7)); */
+
+
+
+function urav(a, b) {
+    if ((a + b) > 10); {
+        return true;
+    }
+}
+console.log(urav(2, 2));
+
+
