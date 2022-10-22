acfa7b387a0c42c2decf27620d79817c1cccc032
olenka10122 <olenka10122@gmail.com>
2022-10-22 09:49:41 +0300

    дом.работа 4.2

--- lesson4/domrab4.js
+++ lesson4/domrab4.js
@@ -35,11 +35,75 @@ const result = 'Понедельник';
 
 
 
-function urav(a, b) {
+/* function urav(a, b) {
     if ((a + b) > 10); {
         return true;
     }
 }
-console.log(urav(2, 2));
+console.log(urav(2, 2)); */
+// не решена//
+
+//№10
+const getDigitsSum = (num)=> {
+    const str = `${num}`;
+    let result = 0;
+    for(let i = 0; i < str.length; i += 1) {
+        result += +str[i]; 
+    }
+    return result;
+}
+console.log(getDigitsSum(123));
+
+
+
+
+
+
+
+//№11
+const filterYears = ()=> {
+    const result = [];
+    for(let i = 1; i < 2020; i+=1){
+        if(getDigitsSum(i) === 13){
+result.push(i); 
+        }
+    }
+    return result;
+}
+console.log(filterYears());
+
+
+//12
+const isEven = (num) => {
+    if (num % 2 === 0) {
+        return true;
+    }
+    return false;
+};
+console.log(isEven(7));
+console.log(isEven(12));
+
+
+//13
+const filterArr = (arr) => {
+    const result =[]
+    for(let i = 0;  i < arr.lenght; i += 1) {
+        if (isEven(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+console.log(filterArr([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+
+const lessNine = (num)=> {
+    console.log(`Функция lessNine вызвана с параметром ${num}`);
+    if(getDigitsSum(num) > 9) {
+        return lessNine(getDigitsSum(num));
+    }
+    return getDigitsSum(num);
+}
+console.log(lessNine(156957565523));
 
 
