66c1ee2ddf87b19aa11ad9f72649948d3eeee2a8
olenka10122 <olenka10122@gmail.com>
2022-11-26 12:49:50 +0300

    1

---
+++ lesson10/index.js
@@ -0,0 +1,9 @@
+/* console.log("script init");
+console.log(window.location); */
+
+const addButton = document.getElementById('add_button');
+console.log(addButton);
+
+
+const buttonCol = document.getElementsByTagName('button');
+console.log(buttonCol);
